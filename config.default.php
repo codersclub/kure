d7cf9e31281e1afa269cb30d4dee85d95383262f
Valery Votintsev <vvotintsev@gmail.com>
2012-12-19 16:11:51 +0400

    config.default.php: replaced "#" comments to ";"

--- config.default.php
+++ config.default.php
@@ -6,32 +6,32 @@
  */
 
 [kure]
-# Name & subtitle of site
+; Name & subtitle of site
 blog_name = kure
 blog_sub  = beta
 
-# Number of entries to display per page
-# Default is 8; no limit is 0
+; Number of entries to display per page
+; Default is 8; no limit is 0
 entries_per_page = 8
 
-# Alphabetize entries? (no to display by most recent)
-# Default is no
+; Alphabetize entries? (no to display by most recent)
+; Default is no
 abc_entries = no
 
-# Markdown is a simple parsing format to pick up on certain cues in plaintext
-# and convert them to their HTML equivalents. Unless you write your own HTML in
-# your entries, you probably want this on.
-# See http://daringfireball.net/projects/markdown/ for more info.
-# Default is yes
+; Markdown is a simple parsing format to pick up on certain cues in plaintext
+; and convert them to their HTML equivalents. Unless you write your own HTML in
+; your entries, you probably want this on.
+; See http://daringfireball.net/projects/markdown/ for more info.
+; Default is yes
 markdown = yes
 
-# Template -- must be a folder in `templates`
-# Default is k1
+; Template -- must be a folder in `templates`
+; Default is k1
 template = k1
 
-# Language -- must be an ini file in `languages`
-# Default is en
+; Language -- must be an ini file in `languages`
+; Default is en
 language = en
 
-# No need to change
+; No need to change
 version =
